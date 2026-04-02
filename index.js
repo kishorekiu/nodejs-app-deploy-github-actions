cf1f24aa154830b2736ee4a804c63674ec87cce5
kishorekiu <kishorekrissh7@gmail.com>
2026-04-02 12:14:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT ?? 5000;
+
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello from the server!!" })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is up and runnning on PORT: ${PORT}`);
+
+})
